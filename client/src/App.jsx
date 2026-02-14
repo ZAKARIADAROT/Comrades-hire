13f0c40e07501d4ed305c48e34f17b1d1af4144d
ZAKARIADAROT <zakariadarot15435@gmail.com>
2026-02-14 20:39:32 +0300

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import ApplyJob from './pages/ApplyJob.jsx'
-import Applications from './pages/applications.jsx'
+import Applications from './pages/Applications.jsx'
 
 const App = () => {
   return (
